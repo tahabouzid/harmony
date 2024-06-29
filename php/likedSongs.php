efe2aadfe5bf34fdc6c5729a77fb4e8a158cf54d
taha Bouzid <taha.lelouch.bouzid@gmail.com>
2024-06-29 20:16:09 +0200

    Your commit message

--- php/likedSongs.php
+++ php/likedSongs.php
@@ -68,7 +68,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
                 }
             }
             echo json_encode($tracks);
-        } else {
+        } else if ( $_GET['fetchdetails'] === 'false'){
             echo json_encode($likedSongs);
         }
     } else {
